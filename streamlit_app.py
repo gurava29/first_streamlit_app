1f5d4048c236d2497c5a26b7b01a36f6c5bfd1a9
gurava29 <115336009+gurava29@users.noreply.github.com>
2023-08-31 18:18:00 +0530

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 streamlit.title('My parents New Healthly Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blue Berry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Harold-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blue Berry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Harold-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
                 
