eda188629a26bf02a74b57efa2932bf86c8a4978
gurava29 <115336009+gurava29@users.noreply.github.com>
2023-08-31 18:13:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My parents New Healthly Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blue Berry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Harold-Boiled Free-Range Egg')
+
+                
