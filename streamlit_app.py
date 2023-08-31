8655dab4ba86dbe77b26ba8b4f4c5bec08d502fe
gurava29 <115336009+gurava29@users.noreply.github.com>
2023-08-31 18:31:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #Displaying the dataframe in the app
-#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
